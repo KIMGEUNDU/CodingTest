8df02df5497399273ef5eb731bf94927c1b14214
KIMGEUNDU <vlkoklv@naver.com>
2023-07-17 17:32:46 +0900

    부분 문자열 확인하기

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log(solution()); //	[1, 3, 5, 7, 9]
-// console.log(solution());
+function solution(my_string, target) {
+  let count = 0;
+
+  if (my_string.includes(target)) {
+    count++;
+  }
+
+  return count;
+}
+
+console.log(solution("banana", "ana")); // 1
+// console.log(solution([58, 44, 27, 10, 100],	139)); //239
