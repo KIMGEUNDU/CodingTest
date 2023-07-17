ad78bdfd4b64d6b742bbdf11da2e443b89920295
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-07-17 17:10:49 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 8.36 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  let newArr = [];
+  for (let key of arr) {
+    if (key % 2 === 0 && key >= 50) {
+      newArr.push(key / 2);
+    } else if (key % 2 === 1 && key < 50) {
+      newArr.push(key * 2);
+    } else {
+      newArr.push(key);
+    }
+  }
+  return newArr;
+}
