9276fb9ade105b1c1b25ab92365d5c690edac207
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-07-17 22:17:21 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,10 +1,24 @@
-function solution(my_string) {
-let num = my_string.match(/\d/g);
-let sum = 0;
+//정규식
+//function solution(my_string) {
+//let num = my_string.match(/\d/g);
+//let sum = 0;
 
-for (let key of num) {
-  sum += parseInt(key);
-}
+//for (let key of num) {
+//  sum += parseInt(key);
+//}
 
-return sum
+//return sum
+//}
+
+//필터
+function solution(my_string){
+    let arr = my_string.split("");
+    let num = arr.filter((key) => key * 1 > 0);
+    let count = 0;
+
+    num.forEach((item) => {
+        count += Number(item);
+    });
+    
+return count
 }
