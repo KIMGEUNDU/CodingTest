cfe7969f3ee34ad2705b120fd0cca247240c847a
KIMGEUNDU <vlkoklv@naver.com>
2023-07-19 01:58:58 +0900

    n번째 원소까지

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function solution(my_string, target) {
-  let count = 0;
-
-  if (my_string.includes(target)) {
-    count++;
-  }
-
-  return count;
-}
-
-console.log(solution("banana", "ana")); // 1
-// console.log(solution([58, 44, 27, 10, 100],	139)); //239
+console.log(solution());
