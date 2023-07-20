25986f1fc7a17a267862632d3e2ddd259f76d58a
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-07-20 21:34:04 +0900

    [unrated] Title: rny_string, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.js
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -1,13 +1,19 @@
-function solution(rny_string) {
-  let arr = Array.from(rny_string);
-  let result = "";
-  arr.forEach((item) => {
-    if (item === "m") {
-      result += "rn";
-    } else {
-      result += item;
-    }
-  });
+//첫번째 방법
+// function solution(rny_string) {
+//   let arr = Array.from(rny_string);
+//   let result = "";
+//   arr.forEach((item) => {
+//     if (item === "m") {
+//       result += "rn";
+//     } else {
+//       result += item;
+//     }
+//   });
 
-  return result;
+//   return result;
+// }
+
+//두번째 방법
+function solution(rny_string) {
+  return rny_string.replace(/[m]/g, "rn");
 }
