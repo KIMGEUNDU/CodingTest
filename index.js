aaf6f0e3f7ac80ac92200e5d9cb196222147df19
KIMGEUNDU <vlkoklv@naver.com>
2023-07-20 21:44:20 +0900

    70번까지 완료

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(solution());
+function solution(num_list) {
+  const [a, b] = [...num_list].reverse();
+  return [...num_list, a > b ? a - b : a * 2];
+}
+
+console.log(solution([2, 1, 6])); //[2, 1, 6, 5]
+console.log(solution([5, 2, 1, 7, 5])); //[5, 2, 1, 7, 5, 10]
