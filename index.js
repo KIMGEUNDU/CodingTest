b2721791fa8139f76fbe2db08e3ac8fd0a1108a8
KIMGEUNDU <vlkoklv@naver.com>
2023-07-21 23:40:47 +0900

    접미사인지 확인하기

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-function solution(num_list) {
-  const [a, b] = [...num_list].reverse();
-  return [...num_list, a > b ? a - b : a * 2];
-}
 
-console.log(solution([2, 1, 6])); //[2, 1, 6, 5]
-console.log(solution([5, 2, 1, 7, 5])); //[5, 2, 1, 7, 5, 10]
+console.log(solution()); //
