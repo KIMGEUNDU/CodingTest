7942a5196d3e63ffc0bd2ad80db13a812e88f2e0
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-08-10 21:28:42 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -2,12 +2,12 @@ function solution(hp) {
   let answer = 0;
 
   if (hp % 5 === 0) {
-    answer += Math.floor(hp / 5);
+    answer += ~~(hp / 5);
   } else if ((hp % 5) % 3 === 0) {
-    answer += Math.floor(hp / 5) + (hp % 5) / 3;
+    answer += ~~(hp / 5) + (hp % 5) / 3;
   } else if (((hp % 5) % 3) % 1 === 0) {
-    answer += Math.floor(hp / 5) + (hp % 5) / 3 + ((hp % 5) % 3) / 1;
+    answer += ~~(hp / 5) + (hp % 5) / 3 + ((hp % 5) % 3) / 1;
   }
 
-  return Math.floor(answer);
+  return ~~(answer);
 }
