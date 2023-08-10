f21401c96e7309f6366a2263fbf9386fdf13cec8
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-08-10 22:15:04 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,13 +1,17 @@
-function solution(my_string, is_prefix) {
-  let arr = [];
-  let result = 0;
-  for (let i = 1; i <= my_string.length; i++) {
-    arr.push(my_string.slice(0, i));
-  }
+// function solution(my_string, is_prefix) {
+//   let arr = [];
+//   let result = 0;
+//   for (let i = 1; i <= my_string.length; i++) {
+//     arr.push(my_string.slice(0, i));
+//   }
 
-  arr.forEach((v) => {
-    v === is_prefix ? result++ : result;
-  });
+//   arr.forEach((v) => {
+//     v === is_prefix ? result++ : result;
+//   });
 
-  return result;
+//   return result;
+// }
+
+function solution(my_string, is_suffix){
+    return my_string.startsWith(is_suffix) ? 1 : 0
 }
