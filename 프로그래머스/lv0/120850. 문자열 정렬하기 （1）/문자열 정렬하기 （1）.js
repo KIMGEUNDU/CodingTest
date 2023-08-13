0048a7464b6d6cf55e7a4b674ec3900d11501351
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-08-14 01:23:49 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,6 +1,10 @@
-function solution(my_string) {
-  return [...my_string]
-    .map((i) => Number(i))
-    .filter((i) => i >= 0)
-    .sort((a, b) => a - b);
+// function solution(my_string) {
+//   return [...my_string]
+//     .map((i) => Number(i))
+//     .filter((i) => i >= 0)
+//     .sort((a, b) => a - b);
+// }
+
+function solution(my_string){
+    return my_string.match(/\d/g).sort((a,b)=>a-b).map((i) => Number(i))
 }
