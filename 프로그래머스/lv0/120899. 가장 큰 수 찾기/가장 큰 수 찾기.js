26f107b9d4294a86fe84f9ba258375ae6033d326
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-08-14 03:03:54 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,7 +1,11 @@
-function solution(array) {
-  let answer = [];
-  let arr1 = [...array].sort((a, b) => b - a);
-  answer.push(arr1[0], array.indexOf(arr1[0]));
+// function solution(array) {
+//   let answer = [];
+//   let arr1 = [...array].sort((a, b) => b - a);
+//   answer.push(arr1[0], array.indexOf(arr1[0]));
 
-  return answer;
+//   return answer;
+// }
+
+function solution(array){
+    return [Math.max(...array), array.indexOf(Math.max(...array))]    
 }
