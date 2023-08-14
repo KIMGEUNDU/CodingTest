d86d335205c6e3c47dc94833ebe4c794c4354a05
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-08-14 22:25:29 +0900

    [unrated] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -1,3 +1,7 @@
+// function solution(todo_list, finished) {
+//   return todo_list.filter((v, i) => (finished[i] === false ? v : ''));
+// }
+
 function solution(todo_list, finished) {
-  return todo_list.filter((v, i) => (finished[i] === false ? v : ''));
+  return todo_list.filter((v, i) => !finished[i]);
 }
