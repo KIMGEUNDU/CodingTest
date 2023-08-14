57ef6978c00a7ba422a8a1189e24402a3396ece5
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-08-14 15:32:43 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,6 +1,11 @@
+// function solution(num_list, n) {
+//   return [
+//     ...num_list.filter((_, i) => i >= n),
+//     ...num_list.filter((_, i) => i < n),
+//   ];
+// }
+
 function solution(num_list, n) {
-  return [
-    ...num_list.filter((_, i) => i >= n),
-    ...num_list.filter((_, i) => i < n),
-  ];
+  num_list.unshift(...num_list.splice(n));
+  return num_list;
 }
