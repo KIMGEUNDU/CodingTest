f78b0fff8f84e7f651812fcc542afbd1c919dcd4
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-08-16 03:06:48 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,5 +1,9 @@
-function solution(arr, delete_list) {
-    const same = arr.filter((v)=> delete_list.indexOf(v) > -1 ).map((v)=> arr.indexOf(v))
-    same.map((v)=> delete arr[v])
-    return arr.filter((v)=> v !== null)
+// function solution(arr, delete_list) {
+//     const same = arr.filter((v)=> delete_list.indexOf(v) > -1 ).map((v)=> arr.indexOf(v))
+//     same.map((v)=> delete arr[v])
+//     return arr.filter((v)=> v !== null)
+// }
+
+function solution(arr, delete_list){
+    return arr.filter((v)=> !delete_list.includes(v))
 }
