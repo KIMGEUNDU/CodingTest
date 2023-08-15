4fa0652e2e5d6e46425758dda596f66a9ad81c24
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2023-08-15 15:51:05 +0900

    [level 0] Title: 369게임, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,5 +1,15 @@
+// function solution(order) {
+//   return String(order)
+//     .split('')
+//     .filter((v) => v % 3 == 0 && v !== '0').length;
+// }
+
+
+2
+3
+4
+5
 function solution(order) {
-  return String(order)
-    .split('')
-    .filter((v) => v % 3 == 0 && v !== '0').length;
+    var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+    return answer;
 }
