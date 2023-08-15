cf709b1da9918b59f134ad75dc227ed8e5663137
KIMGEUNDU <vlkoklv@naver.com>
2023-08-15 19:23:46 +0900

    369게임

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log(solution()); //393
-console.log(solution()); //581
+function solution(my_string) {
+  return Array(my_string);
+}
+
+console.log(solution('banana')); //["a", "ana", "anana", "banana", "na", "nana"]
+console.log(solution('programmers')); //	["ammers", "ers", "grammers", "mers", "mmers", "ogrammers", "programmers", "rammers", "rogrammers", "rs", "s"]
