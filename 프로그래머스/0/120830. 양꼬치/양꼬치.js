ef2b201e295321881e11bab9ea75b229801a0edb
KIMGEUNDU <126174401+KIMGEUNDU@users.noreply.github.com>
2025-01-08 17:30:48 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,6 +1,10 @@
 function solution(n, k) {
-    let sheep = n * 12000
-    let drink = (k * 2000)-(2000 * (Math.floor(n/10)))
-    
-    return sheep + drink
+  let ggocci = n * 12000;
+  let drink = k * 2000;
+
+  if ( n >= 10 ){
+    drink = (k - parseInt(n/10)) * 2000
+  }
+
+  return ggocci + drink;
 }
